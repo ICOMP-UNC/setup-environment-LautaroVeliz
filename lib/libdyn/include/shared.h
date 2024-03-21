b2e23ab509e7c20558ee80cdd7760c8a9423f553
Lautaro Veliz <lautaro.veliz@mi.unc.edu.ar>
2024-03-21 17:10:16 -0300

    CL:
    - Add and link libraries.

---
+++ lib/libdyn/include/shared.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * shared_lib_function
+ * 
+ * This function is defined in the shared library and is
+ * program
+ * 
+ * @param msg: The message tobe printed
+ * @return The message size
+*/
+
+int shared_lib_function(char *msg);
---
+++ lib/libsta/include/static.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * static_lib_function
+ * 
+ * This function is defined in the static library and is
+ * program
+ * 
+ * @param msg: The message tobe printed
+ * @return The message size
+*/
+
+int static_lib_function(char *msg);
